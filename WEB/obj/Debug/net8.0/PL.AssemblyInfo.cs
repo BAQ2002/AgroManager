ef9d5d7ace94241a459b793d54f433e9b5d7f90b
BAQ2002 <antoniobaqueirocosta@gmail.com>
2026-02-28 13:34:58 -0300

    Edição de bovino funcional.

--- WEB/obj/Debug/net8.0/PL.AssemblyInfo.cs
+++ WEB/obj/Debug/net8.0/PL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7ee25c3dc1cb80455aaf46fbe9101aed9cb1b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9929b94810e489f0614d4131c773449160bb8820")]
 [assembly: System.Reflection.AssemblyProductAttribute("PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
