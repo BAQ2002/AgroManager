30712ff1abc53fcb605dd257023d328a98dc93a2
BAQ2002 <antoniobaqueirocosta@gmail.com>
2026-03-01 11:20:10 -0300

    Melhorias na UI de filtros de busca na pagina de View de Bovinos

--- WEB/obj/Debug/net8.0/PL.AssemblyInfo.cs
+++ WEB/obj/Debug/net8.0/PL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d651f92694fe5d0ca6970532fd7564cd204796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9c9cedf13adee06f593d2b73630ebf7e5a0022")]
 [assembly: System.Reflection.AssemblyProductAttribute("PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
