5141ead01511b43d6ee29f033b08de6cf1774660
BAQ2002 <antoniobaqueirocosta@gmail.com>
2026-03-03 06:30:02 -0300

    RollBack

---
+++ WEB/Shared/TopBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PL.Shared
+{
+    public class TopBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
