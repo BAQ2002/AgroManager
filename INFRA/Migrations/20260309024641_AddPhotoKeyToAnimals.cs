b4e25ceb1a162a3ea674d27e0e1bd36489bc1740
lopo-victor <82119199+lopo-victor@users.noreply.github.com>
2026-03-08 23:52:36 -0300

    Nova Migration para contemplar modificacao em AnimalEntity

---
+++ INFRA/Migrations/20260309024641_AddPhotoKeyToAnimals.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INFRA.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoKeyToAnimals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoKey",
+                table: "Swines",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SurrogateMotherFlag",
+                table: "SwineParentages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FatherId",
+                table: "SwineParentages",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoKey",
+                table: "Bovines",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SurrogateMotherFlag",
+                table: "BovineParentages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FatherId",
+                table: "BovineParentages",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoKey",
+                table: "Swines");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoKey",
+                table: "Bovines");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SurrogateMotherFlag",
+                table: "SwineParentages",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FatherId",
+                table: "SwineParentages",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SurrogateMotherFlag",
+                table: "BovineParentages",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FatherId",
+                table: "BovineParentages",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
